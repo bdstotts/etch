a31ceda0ef67dbd099bae71a57e0540dde2d9627
Brody Stotts <150973033+bdstotts@users.noreply.github.com>
2024-01-31 20:16:00 -0700

    create simple grid pattern of boxes whose dimensions can change with a variable

--- script.js
+++ script.js
@@ -3,8 +3,41 @@ const container = document.querySelector('#container');
 //const main = document.createElement('div');
 //main.classList.add("main");
 
-const box = document.createElement("div");
+/*const box = document.createElement("div");
 box.classList.add("box");
-box.textContent = "This is the box to dupe."
+box.textContent = "This is the box to dupe."*/
 
-container.appendChild(box);
+//container.appendChild(box);
+
+let dimension = 6;
+
+/*for (let i=0; i <dimension; i++){
+
+        const firstBox = document.createElement("div");
+        firstBox.classList.add("firstBox");
+        firstBox.textContent = "First Box"
+        container.appendChild(firstBox);
+
+        for (let i=0; i <dimension-1; i++){
+
+            const box = document.createElement("div");
+            box.classList.add("box");
+            box.textContent = "Following Boxes"
+            container.appendChild(box);
+        }
+    }*/
+
+for (let i=0; i<dimension; i++){
+    const rowBox = document.createElement("div");
+    rowBox.classList.add("rowBox");
+    //rowBox.textContent = "This is a row box";
+    container.appendChild(rowBox);
+
+        for(j=0;j<dimension;j++){
+            const box = document.createElement("div");
+            box.classList.add("box");
+            box.textContent = "Following Boxes"
+            rowBox.appendChild(box);
+        }
+
+}
