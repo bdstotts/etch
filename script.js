bc3bfde3a5f3adecef05907f4d9691f1e74cd76b
Brody Stotts <150973033+bdstotts@users.noreply.github.com>
2024-02-02 12:13:50 -0700

    last save state before moving to second branch

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector('#container');
+let box = document.getElementsByClassName("box");
+let dimension = 16;
 
-let dimension = 2;
+
 
 for (let i=0; i<dimension; i++){
     const rowBox = document.createElement("div");
@@ -8,11 +10,26 @@ for (let i=0; i<dimension; i++){
     //rowBox.textContent = "This is a row box";
     container.appendChild(rowBox);
 
-        for(j=0;j<dimension;j++){
-            const box = document.createElement("div");
+       for(j=0;j<dimension;j++){
+            let box = document.createElement("div");
             box.classList.add("box");
-            box.textContent = "box"
+            //box.classList.add("hovered2");
+            box.textContent = "b"
             rowBox.appendChild(box);
-        }
+
+              //console.log(box.);
+       }
 
 }
+
+//console.log(box.classList);
+
+document.getElementsByClassName("box").onmouseover = function()
+{mouseOverBox()};
+function mouseOverBox(){
+    document.getElementsByClassName("box").classList.add("hovered")}
+
+document.getElementById("test").onmouseover = function()
+{mouseOverHeader()};
+function mouseOverHeader(){
+    document.getElementById("test").classList.add("hovered")}
